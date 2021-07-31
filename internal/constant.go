f3ecff33d6a2216e543ec1c36fbe94875c5a99f4
ynwdd <10122431+ynwdd@users.noreply.github.com>
2021-07-31 17:13:21 +0700

    update default

--- internal/constant.go
+++ internal/constant.go
@@ -3,7 +3,7 @@ package internal
 const DOMAIN = "Fastro.app"
 const BUCKET_NAME = "fastro-images"
 const GCS_URL = "https://storage.googleapis.com/fastro-images/"
-const TITLE = "Fastro"
-const DESC = "Iklan Baris Online"
+const TITLE = "Iklan baris online"
+const DESC = "Membantu Anda membuat iklan baris online yang gampang dicari, nyaman dibaca, dan adaptif dengan semua ukuran layar."
 const PROJECT_ID = "fastro-319406"
 const REDIRECT_CODE = 302
