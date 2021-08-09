d9e64662057ec5c89434ba933cc9334863e66d3a
ynwdd <10122431+ynwdd@users.noreply.github.com>
2021-08-09 23:37:56 +0700

    change header

--- internal/page_search.go
+++ internal/page_search.go
@@ -19,8 +19,10 @@ func (p *page) queryPage(req fastrex.Request, res fastrex.Response) {
 	t := query
 	user, _ := p.getUserFromSession(req, res)
 	email := ""
+	initial := ""
 	if user != nil {
 		email = user.Email
+		initial = user.Username[0:1]
 	}
 
 	topic := strings.Title(t)
@@ -30,13 +32,14 @@ func (p *page) queryPage(req fastrex.Request, res fastrex.Response) {
 	date := now.In(loc)
 	desc := "Hasil pencarian berdasarkan topic: " + topic
 	data := struct {
+		Initial     string
 		Email       string
 		Title       string
 		Data        []FlatPost
 		Description string
 		Date        string
 		Domain      string
-	}{email, topic, td, desc, date.Format("2 January 2006"), DOMAIN}
+	}{initial, email, topic, td, desc, date.Format("2 January 2006"), DOMAIN}
 
 	err := res.Render("result", data)
 	if err != nil {
