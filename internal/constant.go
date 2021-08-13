70d0921da44b08eb7d9f5bcb872d3041a698b7a5
ynwdd <10122431+ynwdd@users.noreply.github.com>
2021-08-14 00:18:49 +0700

    PROJECT_ID

--- internal/constant.go
+++ internal/constant.go
@@ -1,9 +1,14 @@
 package internal
 
-const DOMAIN = "Fastro.app"
-const BUCKET_NAME = "fastro-images"
-const GCS_URL = "https://storage.googleapis.com/fastro-images/"
-const TITLE = "Iklan baris online - Beta "
-const DESC = "Membantu Anda membuat iklan baris online yang gampang dicari, nyaman dibaca, ringan, dan responsif."
-const PROJECT_ID = "fastro-319406"
-const REDIRECT_CODE = 302
+import "os"
+
+const (
+	DOMAIN        = "Fastro.app"
+	BUCKET_NAME   = "fastro-images"
+	GCS_URL       = "https://storage.googleapis.com/fastro-images/"
+	TITLE         = "Iklan baris online - Beta "
+	DESC          = "Membantu Anda membuat iklan baris online yang gampang dicari, nyaman dibaca, ringan, dan responsif."
+	REDIRECT_CODE = 302
+)
+
+var PROJECT_ID string = os.Getenv("PROJECT_ID")
