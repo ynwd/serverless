1aca1387f3eb963657c4f44ee1ee65c30b18cdf8
ynwdd <10122431+ynwdd@users.noreply.github.com>
2021-08-21 14:08:20 +0700

    update fastrex

--- go.mod
+++ go.mod
@@ -3,15 +3,15 @@ module github.com/fastrodev/serverless
 go 1.16
 
 require (
-	cloud.google.com/go v0.90.0 // indirect
+	cloud.google.com/go v0.93.3 // indirect
 	cloud.google.com/go/firestore v1.5.0
 	cloud.google.com/go/storage v1.16.0
-	github.com/aws/aws-sdk-go v1.40.17
-	github.com/fastrodev/fastrex v0.0.0-20210728130023-9fc554be4a63
+	github.com/aws/aws-sdk-go v1.40.27
+	github.com/fastrodev/fastrex v0.0.0-20210821060616-43fff58d361a
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/google/uuid v1.3.0
 	github.com/leekchan/accounting v1.0.0
-	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
-	google.golang.org/api v0.52.0
-	google.golang.org/genproto v0.0.0-20210805201207-89edb61ffb67 // indirect
+	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
+	google.golang.org/api v0.54.0
+	google.golang.org/genproto v0.0.0-20210820002220-43fce44e7af1 // indirect
 )
--- internal/template.go
+++ internal/template.go
@@ -3,29 +3,28 @@ package internal
 import "github.com/fastrodev/fastrex"
 
 func createTemplate(app fastrex.App) fastrex.App {
-	app.Template("template/index.gohtml").
-		Template("template/arsip.gohtml").
-		Template("template/signin.gohtml").
-		Template("template/signup.gohtml").
-		Template("template/membership.gohtml").
-		Template("template/detail.gohtml").
-		Template("template/response.gohtml").
-		Template("template/result.gohtml").
-		Template("template/header.gohtml").
-		Template("template/headline.gohtml").
-		Template("template/footer.gohtml").
-		Template("template/meta.gohtml").
-		Template("template/style.gohtml").
-		Template("template/script.gohtml").
-		Template("template/navigation.gohtml").
-		Template("template/style_navigation.gohtml").
-		Template("template/home.gohtml").
-		Template("template/home_header.gohtml").
-		Template("template/home_wrapper.gohtml").
-		Template("template/home_post.gohtml").
-		Template("template/home_post_new.gohtml").
-		Template("template/home_topic.gohtml").
-		Template("template/home_account.gohtml").
-		Template("template/home_setting.gohtml")
-	return app
+	return app.Template("template/index.gohtml",
+		"template/arsip.gohtml",
+		"template/signin.gohtml",
+		"template/signup.gohtml",
+		"template/membership.gohtml",
+		"template/detail.gohtml",
+		"template/response.gohtml",
+		"template/result.gohtml",
+		"template/header.gohtml",
+		"template/headline.gohtml",
+		"template/footer.gohtml",
+		"template/meta.gohtml",
+		"template/style.gohtml",
+		"template/script.gohtml",
+		"template/navigation.gohtml",
+		"template/style_navigation.gohtml",
+		"template/home.gohtml",
+		"template/home_header.gohtml",
+		"template/home_wrapper.gohtml",
+		"template/home_post.gohtml",
+		"template/home_post_new.gohtml",
+		"template/home_topic.gohtml",
+		"template/home_account.gohtml",
+		"template/home_setting.gohtml")
 }
